08487c42d41c658a734fce1d8818059960be5f41
tony-minihum <toshiharuhot@yahoo.co.jp>
2015-07-25 15:34:13 +0900

    test GC.

--- CSharpSandbox/StructList/Program.cs
+++ CSharpSandbox/StructList/Program.cs
@@ -10,11 +10,17 @@ namespace StructList
     {
         public string Str { get; set; }
         public StructSubItem Sub { get; set; }
+        public ClassSubItem ClassSub { get; set; }
 
         public override string ToString()
         {
-            return base.ToString() + " " + Str + " Hash=" + GetHashCode().ToString("x")
-                + " " + Sub.ToString();
+            if (ClassSub == null)
+            {
+                return base.ToString() + " " + Str + " hc=" + GetHashCode().ToString("x")
+                    + " " + Sub.ToString();
+            }
+            return base.ToString() + " " + Str + " hc=" + GetHashCode().ToString("x")
+                + " " + Sub.ToString() + " " + ClassSub.ToString();
         }
     }
 
@@ -24,8 +30,9 @@ namespace StructList
 
         public override string ToString()
         {
-            return base.ToString() + " " + Str + " Hash=" + GetHashCode().ToString("x");
+            return base.ToString() + " " + Str + " hc=" + GetHashCode().ToString("x");
         }
+
     }
 
     class ClassItem
@@ -34,18 +41,24 @@ namespace StructList
         public ClassSubItem Sub { get; set; }
 
         public ClassItem() { }
+        ~ClassItem()
+        {
+            Console.WriteLine("---- destractor : " + this.ToString());
+        }
 
         public override string ToString()
         {
             if (Sub == null)
             {
-                return base.ToString() + " " + Str + " Hash=" + GetHashCode().ToString("x")
+                return base.ToString() + " " + Str + " hc=" + GetHashCode().ToString("x")
                     + " Sub is null.";
             }
 
-            return base.ToString() + " " + Str + " Hash=" + GetHashCode().ToString("x")
+            return base.ToString() + " " + Str + " hc=" + GetHashCode().ToString("x")
                 + " " + Sub.ToString();
         }
+
+
     }
 
     class ClassSubItem
@@ -53,10 +66,14 @@ namespace StructList
         public string Str { get; set; }
 
         public ClassSubItem() { }
+        ~ClassSubItem()
+        {
+            Console.WriteLine("---- destractor : " + this.ToString());
+        }
 
         public override string ToString()
         {
-            return base.ToString() + " " + Str + " Hash=" + GetHashCode().ToString("x");
+            return base.ToString() + " " + Str + " hc=" + GetHashCode().ToString("x");
         }
     }
 
@@ -64,24 +81,40 @@ namespace StructList
     {
         static void Main(string[] args)
         {
-            structItemTest();
-            classItemTest();
+            {
+                var item = structItemTest();
+
+                gc();
+
+                classItemTest(item);
+            }
+
+            gc();
 
             Console.WriteLine("Press any key...");
             Console.ReadKey();
         }
 
-        static void structItemTest()
+        static void gc()
+        {
+            Console.WriteLine("****            ****");
+            Console.WriteLine("**** GC.Collect ****");
+            Console.WriteLine("****            ****");
+            GC.Collect();
+        }
+
+        static ClassSubItem structItemTest()
         {
             var structItems = new List<StructItem>();
 
-            StructSubItem sub = new StructSubItem();
-            sub.Str = "SubItem";
-            StructSubItem sub2 = new StructSubItem();
-            sub2.Str = "SubItem";
-            structItems.Add(new StructItem() { Str = "Item1", Sub = sub });
-            structItems.Add(new StructItem() { Str = "Item2", Sub = sub });
-            structItems.Add(new StructItem() { Str = "Item3", Sub = sub2 });
+            StructSubItem sub = new StructSubItem() { Str = "SubItem" };
+            StructSubItem sub2 = new StructSubItem() { Str = "SubItem" };
+            ClassSubItem cSub = new ClassSubItem() { Str = "ClassSubInStruct" };
+            ClassSubItem cSub2 = new ClassSubItem() { Str = "ClassSubInStruct2" };
+
+            structItems.Add(new StructItem() { Str = "Item1", Sub = sub, ClassSub = cSub});
+            structItems.Add(new StructItem() { Str = "Item2", Sub = sub, ClassSub = cSub });
+            structItems.Add(new StructItem() { Str = "Item3", Sub = sub2, ClassSub = cSub2 });
             structItems.Add(new StructItem() { Str = "Item4", });
             structItems.Add(new StructItem() { Str = "Item5", });
 
@@ -95,20 +128,21 @@ namespace StructList
             {
                 Console.WriteLine(item.ToString());
             }
+
+            return cSub;
         }
 
-        static void classItemTest()
+        static void classItemTest(ClassSubItem cSub)
         {
             var classItems = new List<ClassItem>();
 
-            ClassSubItem sub = new ClassSubItem();
-            sub.Str = "SubItem";
-            ClassSubItem sub2 = new ClassSubItem();
-            sub2.Str = "SubItem";
+            ClassSubItem sub = new ClassSubItem() { Str = "SubItem" };
+            ClassSubItem sub2 = new ClassSubItem() { Str = "SubItem" };
+
             classItems.Add(new ClassItem() { Str = "Item1", Sub = sub });
             classItems.Add(new ClassItem() { Str = "Item2", Sub = sub });
             classItems.Add(new ClassItem() { Str = "Item3", Sub = sub2});
-            classItems.Add(new ClassItem() { Str = "Item4", });
+            classItems.Add(new ClassItem() { Str = "Item4", Sub = cSub});
             classItems.Add(new ClassItem() { Str = "Item5", });
 
 
@@ -117,5 +151,6 @@ namespace StructList
                 Console.WriteLine(item.ToString());
             }
         }
+
     }
 }
